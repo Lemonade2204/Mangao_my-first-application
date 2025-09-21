11330432b871ab774e555b5eca080b7812dbd8e8
unknown <lemuelmangao04@gmail.com>
2025-09-21 22:46:40 +0800

    Chapter 3: Connect to database with Migrations and Eloquent

---
+++ database/migrations/2025_09_21_134806_create_employers_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    // in ..._create_employers_table.php
+    public function up(): void
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Add this line
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employers');
+    }
+};
